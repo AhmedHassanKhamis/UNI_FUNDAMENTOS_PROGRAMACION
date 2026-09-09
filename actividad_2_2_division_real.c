559910e7d8b3d59526cab49b6a54d13032e7b7b1
Ahmed Hassan Khamis <92717109+AhmedHassanKhamis@users.noreply.github.com>
2026-09-09 17:45:59 +0200

    Fix division to use float for accurate results

--- actividad_2_2_division_real.c
+++ actividad_2_2_division_real.c
@@ -10,14 +10,14 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main()
 {
-    //int num1, num2;
-    float num1, num2,resultado;
+    int num1, num2;
+    float resultado;
     printf("Bienvenido al divisioneitor V2!\n");
     printf("Introduce el numero 1:\n");
     scanf("%d", &num1);
     printf("Introduce el numero 2:\n");
     scanf("%d", &num2);
-    resultado = num1 / num2;
+    resultado =(float) num1 / num2;
     printf("El resultado de la division es: %f\n", resultado);
 
     return 0;
